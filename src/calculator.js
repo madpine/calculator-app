b1614db9599e369e6b07026d9bd028892becf68c
madpine <maddiepine96@gmail.com>
2020-12-13 17:59:48 +0200

    Building structure of cards
    Window card, digits card, functions card

--- src/calculator.js
+++ src/calculator.js
@@ -3,9 +3,30 @@ import './App.css';
 
 export default function Calculator() {
     return (
-        <div className="card">
-            <div className="card-body calculator-card">
-                {`Hello calculator`}
+        <div className="card calculator-card">
+            <div className="card-body calculator-body">
+                <div className="card window-card">
+                    <div className="card-body window">
+                        {`Window`}
+                    </div>
+                </div>
+                
+                <div className="row">
+                    <div className="col-8">
+                        <div className="card digit-card">
+                            <div className="card-body digit-body">
+                                {`Digits`}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-4">
+                        <div className="card function-card">
+                            <div className="card-body function-body">
+                                {`Functions`}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
