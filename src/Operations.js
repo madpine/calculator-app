6b7b6246ffcd51c3e9b08bfbfb61cff537335f44
madpine <maddiepine96@gmail.com>
2020-12-19 14:51:57 +0200

    Added and styled operations buttons

--- src/Operations.js
+++ src/Operations.js
@@ -1,5 +1,30 @@
 import React from 'react';
+import './Operations.css';
 
 export default function Operations() {
-    return ("hello operations");
+    return (
+        <div className="operations">
+            <div className="row">
+                <div className="col-6 operations-col">
+                    <button className="operations-button add">{`+`}</button>
+                </div>
+                <div className="col-6 operations-col">
+                    <button className="operations-button subtract">{`-`}</button>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-6 operations-col">
+                    <button className="operations-button multiply">{`x`}</button>
+                </div>
+                <div className="col-6 operations-col">
+                    <button className="operations-button divide">{`÷`}</button>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12 operations-col">
+                    <button className="operations-button equal">{`=`}</button>
+                </div>
+            </div>
+        </div>
+    );
 }
